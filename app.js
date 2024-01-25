d00f3910973a3cbf71850524ff57ef21911c5172
Yuliia Kucheriavenko <kucherisvenko@gmail.com>
2024-01-25 14:26:10 +0200

    application implemented task

---
+++ app.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const faqs = document.querySelectorAll(".faq");
+  const searches = document.querySelectorAll(".search");
+
+  faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+      faq.classList.toggle("active");
+    });
+  });
+
+  searches.forEach(search => {
+    search.addEventListener("click", () => {
+      search.classList.toggle("active");
+    });
+  });
+});
+
+document.getElementById('logo').addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
